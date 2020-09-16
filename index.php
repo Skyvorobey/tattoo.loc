ad9309b57a2e95b21197d0a0576560fba3a4dd46
Sky <skyvorobey@icloud.com>
2020-09-16 09:49:27 +0200

    Сделал 1\3 (section-about)

--- index.php
+++ index.php
@@ -3,6 +3,37 @@ $main = 'home';
 include __DIR__. '/other_pages/head.php';
 include __DIR__. '/other_pages/nav.php';
 include __DIR__. '/other_pages/banner.php'?>
-
+<section class="section-about">
+    <div class="section-services">
+        <div class="section-services__item">
+            <div class="section-services__logo section-services__logo--back1"></div>
+            <h3 class="section-services__name">Тату</h3>
+            <button class="header__item btn-back-call btn-back-call--top65">
+                <a href="/index.php">Подробнее</a>
+            </button>
+        </div>
+        <div class="section-services__item">
+            <div class="section-services__logo section-services__logo--back2"></div>
+            <h3 class="section-services__name">Пирсинг</h3>
+            <button class="header__item btn-back-call btn-back-call--top65">
+                <a href="/index.php">Подробнее</a>
+            </button>
+        </div>
+        <div class="section-services__item">
+            <div class="section-services__logo section-services__logo--back3"></div>
+            <h3 class="section-services__name">Татуаж</h3>
+            <button class="header__item btn-back-call btn-back-call--top65">
+                <a href="/index.php">Подробнее</a>
+            </button>
+        </div>
+        <div class="section-services__item">
+            <div class="section-services__logo section-services__logo--back4"></div>
+            <h3 class="section-services__name">Обучение</h3>
+            <button class="header__item btn-back-call btn-back-call--top65">
+                <a href="/index.php">Подробнее</a>
+            </button>
+        </div>
+    </div>
+</section>
 
 <?php include __DIR__. '/other_pages/footer.php'; ?>
